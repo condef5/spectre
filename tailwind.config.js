f9bb21235c87fbeb9071dc57e03fc13be353e334
condef5 <frank@able.co>
2020-02-01 17:39:34 -0500

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    colors: {
+      white: "#ffffff",
+      cyan: {
+        default: "cyan",
+        dark: "darkcyan"
+      },
+      tint: {
+        "01": "#333"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
