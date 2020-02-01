87426c1eae86de42ced6a7234bd40ddd7b1b6e5e
condef5 <frank@able.co>
2020-02-01 14:58:51 -0500

    Link start

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,5 @@
-import React from 'react'
-import Link from 'next/link'
-
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => ({
-  ...link,
-  key: `nav-link-${link.href}-${link.label}`,
-}))
+import React from "react";
+import Link from "next/link";
 
 const Nav = () => (
   <nav>
@@ -17,16 +9,17 @@ const Nav = () => (
           <a>Home</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
+      <li>
+        <Link href="https://github.com/condef5/spectre">
+          <a>Github</a>
+        </Link>
+      </li>
     </ul>
 
     <style jsx>{`
       :global(body) {
         margin: 0;
+        background: #000;
         font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
           Helvetica, sans-serif;
       }
@@ -45,12 +38,17 @@ const Nav = () => (
         padding: 6px 8px;
       }
       a {
-        color: #067df7;
+        color: darkcyan;
         text-decoration: none;
+        font-weight: 600;
         font-size: 13px;
+        letter-spacing: 0.5px;
+      }
+      a:hover {
+        color: cyan;
       }
     `}</style>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
 
 const Home = () => (
   <div>
@@ -12,28 +12,10 @@ const Home = () => (
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
+      <h1 className="title">Welcome to Spectre!</h1>
       <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
+        The best site for recommendations on anime, cartoons and manga.
       </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
     </div>
 
     <style jsx>{`
@@ -43,46 +25,21 @@ const Home = () => (
       }
       .title {
         margin: 0;
+        color: #fff;
         width: 100%;
         padding-top: 80px;
         line-height: 1.15;
         font-size: 48px;
       }
+      .description {
+        color: darkcyan;
+      }
       .title,
       .description {
         text-align: center;
       }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
